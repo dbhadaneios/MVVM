cec1f230422ae2c88fc91802101b4d39c80be7b4
dbhadaneios <121401902+dbhadaneios@users.noreply.github.com>
2022-12-25 18:57:36 +0000

    Initial Project + Network Layer Added

---
+++ MVVM/MVVM/Helper/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  MVVM
+//
+//  Created by Dinesh R Bhaddane on 25/12/2022.
+//
+
+import Foundation
+class Observable<T> {
+
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+
+    private var listener: ((T) -> Void)?
+
+    init(_ value: T) {
+        self.value = value
+    }
+
+    func bind(_ closure: @escaping (T) -> Void) {
+        closure(value)
+        listener = closure
+    }
+}
---
+++ MVVM/MVVM/NetworkManager/NetworkConstants.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkConstants.swift
+//  MVVM
+//
+//  Created by Dinesh R Bhaddane on 25/12/2022.
+//
+
+import Foundation
+struct NetworkConstants {
+    static let invalidURL = "Invalid URL"
+    static let responseError = "Invalid response"
+    static let somethingWentWrong = "Something went wrong, please try again later"
+    static let decodeError = "Failed to parse data."
+    static let unreachable = "Please check your internet connection."
+}
