7131e2cbe2563db420af14a7d3dcef245a1e5476
Dinesh R Bhaddane <dinesh.rajkishor@lloydsbanking.com>
2022-12-30 21:34:20 +0000

    Unit test cases added

---
+++ MVVM/MVVMTests/BooksMockService.swift
@@ -0,0 +1,69 @@
+//
+//  BooksMockService.swift
+//  MVVMTests
+//
+//  Created by Dinesh R Bhaddane on 28/12/2022.
+//
+
+import Foundation
+@testable import MVVM
+
+enum ResponseFileName: String {
+    case success = "books"
+    case failure = "books_error"
+    case noData = "books_empty"
+}
+/// Mock service
+class BooksMockService: BooksServiceProtocol {
+    /// variable for caching downlaoded images
+    private let imageCache = NSCache<NSString, NSData>()
+    var responseFileName: ResponseFileName?
+    
+    private(set) var isBooksServiceMethodCalled = false
+    private(set) var isImageServiceMethodCalled = false
+
+    init(responseFileName: ResponseFileName?) {
+        self.responseFileName = responseFileName
+    }
+    /// Function to get list of books from  mock json
+    /// - Parameters:
+    ///   - endPoint:api end point url
+    ///   - completionHandler: returns result types which includes success(Books) and failure data
+    func booksService(completionHandler: @escaping ((Result<Books, NetworkError>) -> Void)) {
+        isBooksServiceMethodCalled = true
+        guard
+            let url = Bundle.main.url(forResource: responseFileName?.rawValue, withExtension: "json"),
+             let data = try? Data(contentsOf: url)
+        else {
+             completionHandler(.failure(.somethingWentWrong))
+             return
+        }
+        NetworkManager.shared.decode(data, completionHandler: completionHandler)
+    }
+    
+    /// mock image service
+    /// - Parameters:
+    ///   - urlString: image url
+    ///   - completionHandler: returns result types which includes success(image) and failure data
+    func downloadImageService(withUrl urlString: String, completionHandler: @escaping ((Result<Data, NetworkError>) -> Void)) {
+        isImageServiceMethodCalled = true
+        guard URL(string: urlString) != nil else {
+            completionHandler(.failure(.invalidURL))
+            return
+        }
+        /// check image already available in cache
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
+            completionHandler(.success(cachedImage as Data))
+            return
+        }
+        guard let data = urlString.data(using: .utf8) else {
+            completionHandler(.failure(.responseError))
+            return
+        }
+        
+        /// store image in cache
+        self.imageCache.setObject(data as NSData, forKey: urlString as NSString)
+        completionHandler(.success(data))
+    }
+
+}
---
+++ MVVM/MVVMTests/BooksViewModelTests.swift
@@ -0,0 +1,102 @@
+//
+//  BooksViewModelTests.swift
+//  MVVMTests
+//
+//  Created by Dinesh R Bhaddane on 28/12/2022.
+//
+
+import XCTest
+@testable import MVVM
+
+class BooksViewModelTests: XCTestCase {
+    
+    var booksViewModel: BooksViewModel?
+    var bookServiceManager: BooksMockService!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        booksViewModel = nil
+        bookServiceManager = nil
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    /// Test mock service
+    func testGetBooks() {
+        let requestExpectation = expectation(description: "Request should finish with Books")
+        bookServiceManager = BooksMockService(responseFileName: .success)
+        booksViewModel = BooksViewModel(serviceManager: bookServiceManager)
+
+        booksViewModel?.getBooks()
+        XCTAssertTrue(bookServiceManager.isBooksServiceMethodCalled, "bookServiceManager method should be called.")
+        booksViewModel?.booksCellViewModels.bind { booksCellViewModel in
+            XCTAssertEqual(booksCellViewModel.count, 6, "its should equal to mock data of count 6")
+            XCTAssertNotNil(booksCellViewModel[0].title)
+            XCTAssertNotNil(booksCellViewModel[0].subtitle)
+            requestExpectation.fulfill()
+
+        }
+        wait(for: [requestExpectation], timeout: 3)
+    }
+    
+    /// Test mock service failure case
+    func testGetBooksFailureCase() {
+        let requestExpectation = expectation(description: "Request should finish with error")
+        bookServiceManager = BooksMockService(responseFileName: .failure)
+        booksViewModel = BooksViewModel(serviceManager: bookServiceManager)
+        booksViewModel?.getBooks()
+        XCTAssertTrue(bookServiceManager.isBooksServiceMethodCalled, "bookServiceManager method should be called.")
+        booksViewModel?.error.bind { error in
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error, "Something went wrong, please try again later")
+            requestExpectation.fulfill()
+        }
+        wait(for: [requestExpectation], timeout: 3)
+    }
+    
+    /// Test empty mock response
+    func testGetEmptyResponse() {
+        let requestExpectation = expectation(description: "Request should finish with empty response")
+        bookServiceManager = BooksMockService(responseFileName: .noData)
+        booksViewModel = BooksViewModel(serviceManager: bookServiceManager)
+        booksViewModel?.getBooks()
+        XCTAssertTrue(bookServiceManager.isBooksServiceMethodCalled, "bookServiceManager method should be called.")
+        booksViewModel?.booksCellViewModels.bind { booksCellViewModel in
+            XCTAssertEqual(booksCellViewModel.count, 0, "it should be no data")
+            requestExpectation.fulfill()
+        }
+        wait(for: [requestExpectation], timeout: 3)
+    }
+    
+    /// Test success mock response data
+    func testBooksResponse() {
+        let requestExpectation = expectation(description: "Request should finish with correct response")
+        bookServiceManager = BooksMockService(responseFileName: .success)
+        booksViewModel = BooksViewModel(serviceManager: bookServiceManager)
+        booksViewModel?.getBooks()
+        XCTAssertTrue(bookServiceManager.isBooksServiceMethodCalled, "bookServiceManager method should be called.")
+        booksViewModel?.booksCellViewModels.bind { booksCellViewModel in
+            
+            XCTAssertEqual(booksCellViewModel[0].title, "The India Story")
+            XCTAssertEqual(booksCellViewModel[0].subtitle, "Over a period of five years, The India Story has grown to become one of the hippest cultural events in Kolkata.")
+            XCTAssertEqual(booksCellViewModel[0].imageUrString, "https://png.pngtree.com/png-vector/20190505/ourmid/pngtree-vector-book-icon-png-image_1022249.jpg")
+            requestExpectation.fulfill()
+        }
+        wait(for: [requestExpectation], timeout: 3)
+    }
+}
+
+class MockDelegate: BooksDataViewModelDelegate {
+    var delegateMethodCalled = false
+    var imageData: Data?
+    var error: String?
+
+    func didImageDownload(data: Data?, error: String?) {
+        delegateMethodCalled = true
+        imageData = data
+        self.error = error
+    }
+    
+}
